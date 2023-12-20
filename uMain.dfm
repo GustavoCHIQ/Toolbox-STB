14c8cb246bba8a1cb61fa38205f34a2c91b9a8b7
Gustavo Chiquitin <53371023+GustavoCHIQ@users.noreply.github.com>
2023-12-20 14:16:52 -0400

    Initial Commit

---
+++ uMain.dfm
@@ -0,0 +1,392 @@
+object frmPrincipal: TfrmPrincipal
+  Left = 0
+  Top = 0
+  BorderIcons = [biSystemMenu, biMinimize]
+  BorderStyle = bsSingle
+  Caption = 'ToolBox for Android STB - Beta'
+  ClientHeight = 741
+  ClientWidth = 878
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  Position = poMainFormCenter
+  OnClose = FormClose
+  OnDestroy = FormDestroy
+  OnShow = FormShow
+  TextHeight = 15
+  object pnlPrincipal: TPanel
+    Left = 0
+    Top = 0
+    Width = 878
+    Height = 741
+    Align = alClient
+    TabOrder = 0
+    ExplicitWidth = 876
+    ExplicitHeight = 733
+    object pnlMeio: TPanel
+      Left = 1
+      Top = 121
+      Width = 876
+      Height = 376
+      Align = alTop
+      Locked = True
+      TabOrder = 1
+      Visible = False
+      ExplicitWidth = 874
+      object pnlFuncoes: TPanel
+        Left = 1
+        Top = 1
+        Width = 874
+        Height = 152
+        Align = alTop
+        TabOrder = 0
+        ExplicitWidth = 872
+        object gpBoxFuncoes: TGroupBox
+          Left = 1
+          Top = 1
+          Width = 872
+          Height = 398
+          Align = alTop
+          Caption = 'Fun'#231#245'es'
+          Font.Charset = DEFAULT_CHARSET
+          Font.Color = clWindowText
+          Font.Height = -17
+          Font.Name = 'Segoe UI'
+          Font.Style = []
+          ParentFont = False
+          TabOrder = 0
+          ExplicitWidth = 870
+          object btnReiniciarBox: TButton
+            Left = 15
+            Top = 32
+            Width = 186
+            Height = 41
+            Caption = 'Reiniciar TV BOX'
+            TabOrder = 0
+            StyleName = 'Windows'
+            OnClick = btnReiniciarBoxClick
+          end
+          object btnTirarPrint: TButton
+            Left = 15
+            Top = 81
+            Width = 186
+            Height = 41
+            Caption = 'Tirar Print da Tela'
+            TabOrder = 1
+            StyleName = 'Windows'
+            OnClick = btnTirarPrintClick
+          end
+          object bntLimparDados: TButton
+            Left = 207
+            Top = 32
+            Width = 186
+            Height = 41
+            Caption = 'Limpar Dados do APP'
+            TabOrder = 2
+            StyleName = 'Windows'
+            OnClick = bntLimparDadosClick
+          end
+          object btnInput: TButton
+            Left = 207
+            Top = 81
+            Width = 186
+            Height = 41
+            Caption = 'Inserir Texto'
+            TabOrder = 3
+            StyleName = 'Windows'
+            OnClick = btnInputClick
+          end
+          object edtTexto: TEdit
+            Left = 416
+            Top = 86
+            Width = 260
+            Height = 31
+            TabOrder = 4
+          end
+        end
+        object cbbPacotes: TComboBox
+          Left = 417
+          Top = 40
+          Width = 265
+          Height = 28
+          AutoComplete = False
+          DropDownCount = 10
+          Font.Charset = DEFAULT_CHARSET
+          Font.Color = clWindowText
+          Font.Height = -15
+          Font.Name = 'Segoe UI'
+          Font.Style = []
+          ParentFont = False
+          TabOrder = 1
+          StyleName = 'Windows'
+        end
+      end
+      object pnlControles: TPanel
+        Left = 1
+        Top = 153
+        Width = 874
+        Height = 222
+        Align = alClient
+        TabOrder = 1
+        ExplicitWidth = 872
+        object gpBoxControles: TGroupBox
+          Left = 1
+          Top = 1
+          Width = 872
+          Height = 216
+          Align = alTop
+          Caption = 'Controles'
+          Font.Charset = DEFAULT_CHARSET
+          Font.Color = clWindowText
+          Font.Height = -17
+          Font.Name = 'Segoe UI'
+          Font.Style = []
+          ParentFont = False
+          TabOrder = 0
+          ExplicitWidth = 870
+          object btnCima: TButton
+            Left = 134
+            Top = 48
+            Width = 97
+            Height = 41
+            Caption = 'CIMA'
+            TabOrder = 0
+            StyleName = 'Windows'
+            OnClick = btnCimaClick
+          end
+          object btnBaixo: TButton
+            Left = 134
+            Top = 142
+            Width = 97
+            Height = 41
+            Caption = 'BAIXO'
+            TabOrder = 1
+            StyleName = 'Windows'
+            OnClick = btnBaixoClick
+          end
+          object btnEsqueda: TButton
+            Left = 31
+            Top = 95
+            Width = 97
+            Height = 41
+            Caption = 'ESQUERDA'
+            TabOrder = 2
+            StyleName = 'Windows'
+            OnClick = btnEsquedaClick
+          end
+          object btnDireita: TButton
+            Left = 237
+            Top = 95
+            Width = 97
+            Height = 41
+            Caption = 'DIREITA'
+            TabOrder = 3
+            StyleName = 'Windows'
+            OnClick = btnDireitaClick
+          end
+          object bntVoltar: TButton
+            Left = 463
+            Top = 56
+            Width = 97
+            Height = 41
+            Caption = 'VOLTAR'
+            TabOrder = 4
+            StyleName = 'Windows'
+            OnClick = bntVoltarClick
+          end
+          object btnOK: TButton
+            Left = 134
+            Top = 95
+            Width = 97
+            Height = 41
+            Caption = 'OK'
+            TabOrder = 5
+            StyleName = 'Windows'
+            OnClick = btnOKClick
+          end
+          object btnInicio: TButton
+            Left = 360
+            Top = 56
+            Width = 97
+            Height = 41
+            Caption = 'INICIO'
+            TabOrder = 6
+            StyleName = 'Windows'
+            OnClick = btnInicioClick
+          end
+          object btnProximo: TButton
+            Left = 463
+            Top = 134
+            Width = 97
+            Height = 41
+            Caption = 'PROXIMO'
+            TabOrder = 7
+            StyleName = 'Windows'
+            OnClick = btnProximoClick
+          end
+          object btnIr: TButton
+            Left = 360
+            Top = 134
+            Width = 97
+            Height = 41
+            Caption = 'IR'
+            TabOrder = 8
+            StyleName = 'Windows'
+            OnClick = btnIrClick
+          end
+          object btnVolumeMais: TButton
+            Left = 570
+            Top = 56
+            Width = 166
+            Height = 41
+            Caption = 'Aumentar Volume'
+            TabOrder = 9
+            StyleName = 'Windows'
+            OnClick = btnVolumeMaisClick
+          end
+          object btnVolumesMenos: TButton
+            Left = 570
+            Top = 134
+            Width = 166
+            Height = 41
+            Caption = 'Diminuir Volume'
+            TabOrder = 10
+            StyleName = 'Windows'
+            OnClick = btnVolumesMenosClick
+          end
+          object Button1: TButton
+            Left = 616
+            Top = 96
+            Width = 75
+            Height = 39
+            Caption = 'Mutar'
+            TabOrder = 11
+            StyleName = 'Windows'
+          end
+        end
+      end
+    end
+    object pnlTopo: TPanel
+      Left = 1
+      Top = 1
+      Width = 876
+      Height = 120
+      Align = alTop
+      TabOrder = 0
+      ExplicitWidth = 874
+      object GroupBox2: TGroupBox
+        Left = 1
+        Top = 1
+        Width = 874
+        Height = 118
+        Align = alClient
+        Caption = 'Conex'#227'o'
+        Font.Charset = DEFAULT_CHARSET
+        Font.Color = clWindowText
+        Font.Height = -17
+        Font.Name = 'Segoe UI'
+        Font.Style = []
+        ParentFont = False
+        TabOrder = 0
+        ExplicitWidth = 872
+        object edtIPBOX: TEdit
+          Left = 31
+          Top = 38
+          Width = 329
+          Height = 49
+          Font.Charset = DEFAULT_CHARSET
+          Font.Color = clWindowText
+          Font.Height = -30
+          Font.Name = 'Segoe UI'
+          Font.Style = []
+          ParentFont = False
+          TabOrder = 0
+          TextHint = 'IP de Acesso'
+          StyleName = 'Windows'
+        end
+        object btnConectar: TButton
+          Left = 439
+          Top = 38
+          Width = 122
+          Height = 49
+          Caption = 'Conectar'
+          Font.Charset = DEFAULT_CHARSET
+          Font.Color = clWindowText
+          Font.Height = -17
+          Font.Name = 'Segoe UI'
+          Font.Style = []
+          ParentFont = False
+          TabOrder = 1
+          StyleName = 'Windows'
+          OnClick = btnConectarClick
+        end
+        object btnDesconectar: TButton
+          Left = 608
+          Top = 38
+          Width = 129
+          Height = 49
+          Caption = 'Desconectar'
+          Font.Charset = DEFAULT_CHARSET
+          Font.Color = clWindowText
+          Font.Height = -18
+          Font.Name = 'Segoe UI'
+          Font.Style = []
+          ParentFont = False
+          TabOrder = 2
+          StyleName = 'Windows'
+          OnClick = btnDesconectarClick
+        end
+      end
+    end
+    object pnlInferior: TPanel
+      Left = 1
+      Top = 497
+      Width = 876
+      Height = 243
+      Align = alClient
+      TabOrder = 2
+      ExplicitWidth = 874
+      ExplicitHeight = 235
+      object StatusBar1: TStatusBar
+        Left = 1
+        Top = 216
+        Width = 874
+        Height = 26
+        Panels = <
+          item
+            Alignment = taRightJustify
+            Width = 200
+          end>
+        ExplicitTop = 208
+        ExplicitWidth = 872
+      end
+      object mmoLog: TMemo
+        Left = 1
+        Top = 1
+        Width = 874
+        Height = 215
+        Align = alClient
+        Font.Charset = DEFAULT_CHARSET
+        Font.Color = clWindowText
+        Font.Height = -23
+        Font.Name = 'Segoe UI'
+        Font.Style = []
+        ParentFont = False
+        ReadOnly = True
+        ScrollBars = ssVertical
+        TabOrder = 1
+        StyleName = 'Windows'
+        ExplicitWidth = 872
+        ExplicitHeight = 207
+      end
+    end
+  end
+  object Timer1: TTimer
+    OnTimer = Timer1Timer
+    Left = 794
+    Top = 42
+  end
+end
---
+++ uMain.pas
@@ -0,0 +1,407 @@
+﻿unit uMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls, ShellAPI,
+  dprocess, IconFontsImage, System.ImageList, Vcl.ImgList, Vcl.ComCtrls,
+  System.JSON, System.Generics.Collections,
+  System.IOUtils;
+
+type
+  TfrmPrincipal = class(TForm)
+    pnlPrincipal: TPanel;
+    btnConectar: TButton;
+    edtIPBOX: TEdit;
+    mmoLog: TMemo;
+    btnReiniciarBox: TButton;
+    gpBoxFuncoes: TGroupBox;
+    btnTirarPrint: TButton;
+    bntLimparDados: TButton;
+    pnlTopo: TPanel;
+    gpBoxControles: TGroupBox;
+    GroupBox2: TGroupBox;
+    pnlMeio: TPanel;
+    pnlFuncoes: TPanel;
+    pnlControles: TPanel;
+    StatusBar1: TStatusBar;
+    pnlInferior: TPanel;
+    Timer1: TTimer;
+    btnDesconectar: TButton;
+    cbbPacotes: TComboBox;
+    btnInicio: TButton;
+    btnInput: TButton;
+    edtTexto: TEdit;
+    btnProximo: TButton;
+    btnIr: TButton;
+    btnVolumeMais: TButton;
+    btnVolumesMenos: TButton;
+    Button1: TButton;
+    procedure btnConectarClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure btnReiniciarBoxClick(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure btnDesconectarClick(Sender: TObject);
+    procedure btnTirarPrintClick(Sender: TObject);
+    procedure bntLimparDadosClick(Sender: TObject);
+    procedure btnCimaClick(Sender: TObject);
+    procedure btnDireitaClick(Sender: TObject);
+    procedure btnBaixoClick(Sender: TObject);
+    procedure btnEsquedaClick(Sender: TObject);
+    procedure btnOKClick(Sender: TObject);
+    procedure bntVoltarClick(Sender: TObject);
+    procedure btnInicioClick(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure btnInputClick(Sender: TObject);
+    procedure btnProximoClick(Sender: TObject);
+    procedure btnIrClick(Sender: TObject);
+    procedure btnVolumeMaisClick(Sender: TObject);
+    procedure btnVolumesMenosClick(Sender: TObject);
+  private
+    { Private declarations }
+    procedure PreencherComboBoxComArquivoJSON(const FileName: string);
+    function ExtractPacoteFromComboBox(const ComboBoxText: string): string;
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPrincipal: TfrmPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmPrincipal.btnDesconectarClick(Sender: TObject);
+var
+  output: AnsiString;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['disconnect'], output);
+  mmoLog.Lines.Add(Trim(output));
+
+  btnConectar.Enabled := Enabled;
+  btnDesconectar.Enabled := not Enabled;
+  pnlMeio.visible := False;
+  edtIPBOX.Text := '';
+end;
+
+procedure TfrmPrincipal.btnDireitaClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'keyevent',
+    '22'], output);
+  mmoLog.Lines.Add(Trim(output));
+  mmoLog.Lines.Add('Comando executado com sucesso !');
+end;
+
+procedure TfrmPrincipal.btnEsquedaClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'keyevent',
+    '21'], output);
+  mmoLog.Lines.Add(Trim(output));
+  mmoLog.Lines.Add('Comando executado com sucesso !');
+end;
+
+procedure TfrmPrincipal.btnInicioClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'keyevent',
+    '3'], output);
+  mmoLog.Lines.Add(Trim(output));
+  mmoLog.Lines.Add('Comando executado com sucesso !');
+end;
+
+procedure TfrmPrincipal.btnInputClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  if edtTexto.Text = '' then
+  begin
+    ShowMessage('Digite um texto para ser enviado!');
+    edtTexto.SetFocus;
+    Exit;
+  end;
+
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'text',
+    edtTexto.Text], output);
+  mmoLog.Lines.Add('Texto enviado com sucesso !');
+
+  // Limpar o edit
+  edtTexto.Text := '';
+end;
+
+procedure TfrmPrincipal.btnIrClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'keyevent',
+    '66'], output);
+  mmoLog.Lines.Add(Trim(output));
+  mmoLog.Lines.Add('Comando executado com sucesso !');
+end;
+
+procedure TfrmPrincipal.btnOKClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'keyevent',
+    '23'], output);
+  mmoLog.Lines.Add(Trim(output));
+  mmoLog.Lines.Add('Comando executado com sucesso !');
+end;
+
+procedure TfrmPrincipal.btnReiniciarBoxClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['reboot'], output);
+  // aguardar 15 segundos para reiniciar e desativar o botão de reiniciar
+  Sleep(15000);
+  mmoLog.Lines.Add(Trim(output));
+end;
+
+procedure TfrmPrincipal.btnTirarPrintClick(Sender: TObject);
+var
+  output: ansistring;
+  byteArray: TBytes;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['exec-out', 'screencap',
+    '-p'], output);
+
+  // Converter a AnsiString para TBytes
+  SetLength(byteArray, Length(output));
+  Move(output[1], byteArray[0], Length(output));
+
+  // Salvar o print na pasta do projeto
+  TFile.WriteAllBytes('print.png', byteArray);
+
+  mmoLog.Lines.Add('Print salvo no diretório: ' + GetCurrentDir);
+
+  // Criar MessageDlg personalizado
+  if MessageBox(Handle, 'Deseja abrir o print?', 'Abrir Print', MB_YESNO) = IDYES
+  then
+    ShellExecute(Handle, 'open', 'print.png', nil, nil, SW_SHOWNORMAL);
+end;
+
+procedure TfrmPrincipal.btnVolumeMaisClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'keyevent',
+    '24'], output);
+  mmoLog.Lines.Add(Trim(output));
+  mmoLog.Lines.Add('Comando executado com sucesso !');
+end;
+
+procedure TfrmPrincipal.btnVolumesMenosClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'keyevent',
+    '25'], output);
+  mmoLog.Lines.Add(Trim(output));
+  mmoLog.Lines.Add('Comando executado com sucesso !');
+end;
+
+procedure TfrmPrincipal.btnProximoClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'keyevent',
+    '61'], output);
+  mmoLog.Lines.Add(Trim(output));
+  mmoLog.Lines.Add('Comando executado com sucesso !');
+end;
+
+function TfrmPrincipal.ExtractPacoteFromComboBox(const ComboBoxText
+  : string): string;
+var
+  StartPos, EndPos: Integer;
+begin
+  // Extrair o valor do pacote entre parênteses
+  StartPos := Pos('(', ComboBoxText);
+  EndPos := Pos(')', ComboBoxText);
+
+  if (StartPos > 0) and (EndPos > StartPos) then
+    Result := Trim(Copy(ComboBoxText, StartPos + 1, EndPos - StartPos - 1))
+  else
+    Result := '';
+end;
+
+procedure TfrmPrincipal.bntLimparDadosClick(Sender: TObject);
+var
+  output: ansistring;
+  PacoteSelecionado: string;
+begin
+  // Obter o valor do pacote da seleção do ComboBox
+  PacoteSelecionado := ExtractPacoteFromComboBox(cbbPacotes.Text);
+
+  if PacoteSelecionado = '' then
+  begin
+    ShowMessage('Selecione um pacote para limpar os dados!');
+    cbbPacotes.SetFocus;
+    Exit;
+  end;
+
+  // Deseja limpar os dados do pacote selecionado?
+  if MessageBox(Handle, 'Deseja limpar os dados do pacote selecionado?',
+    'Limpar Dados', MB_YESNO) = IDNO then
+    Exit;
+
+  // Comando ADB para limpar os dados do pacote
+  if RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'pm', 'clear',
+    PacoteSelecionado], output) then
+  begin
+    mmoLog.Lines.Add('Dados do pacote ' + PacoteSelecionado +
+      ' limpos com sucesso.');
+    // limpar combo box
+    cbbPacotes.ItemIndex := -1;
+  end
+  else
+    mmoLog.Lines.Add('Erro ao limpar os dados do pacote ' + PacoteSelecionado +
+      ': ' + Trim(output));
+end;
+
+procedure TfrmPrincipal.bntVoltarClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'keyevent',
+    '4'], output);
+  mmoLog.Lines.Add(Trim(output));
+  mmoLog.Lines.Add('Comando executado com sucesso !');
+end;
+
+procedure TfrmPrincipal.btnBaixoClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'keyevent',
+    '20'], output);
+  mmoLog.Lines.Add(Trim(output));
+  mmoLog.Lines.Add('Comando executado com sucesso !');
+end;
+
+procedure TfrmPrincipal.btnCimaClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['shell', 'input', 'keyevent',
+    '19'], output);
+  mmoLog.Lines.Add(Trim(output));
+  mmoLog.Lines.Add('Comando executado com sucesso !');
+end;
+
+procedure TfrmPrincipal.btnConectarClick(Sender: TObject);
+var
+  output: ansistring;
+begin
+  mmoLog.Lines.Clear;
+  mmoLog.Font.Size := 14;
+  mmoLog.Alignment := taLeftJustify;
+  // passando o IP digitado no edit
+  RuncOmmand('C:\platform-tools\adb.exe',
+    ['connect', edtIPBOX.Text + ':5555'], output);
+  mmoLog.Lines.Add(Trim(output));
+
+  // verificar se no output possui somente o texto "connected to + ip"
+  if Pos('connected to', output) > 0 then
+  begin
+    // ativar o pnlMeio
+    pnlMeio.visible := True;
+    btnConectar.Enabled := not Enabled;
+    btnDesconectar.Enabled := Enabled;
+  end;
+end;
+
+procedure TfrmPrincipal.FormClose(Sender: TObject; var Action: TCloseAction);
+var
+  output: ansistring;
+begin
+  RuncOmmand('C:\platform-tools\adb.exe', ['disconnect'], output);
+end;
+
+procedure TfrmPrincipal.FormDestroy(Sender: TObject);
+begin
+  cbbPacotes.Items.Clear;
+end;
+
+procedure TfrmPrincipal.FormShow(Sender: TObject);
+begin
+  btnDesconectar.Enabled := not Enabled;
+  mmoLog.Font.Size := 50;
+  // centralizar o texto do memo
+  mmoLog.Alignment := taCenter;
+  mmoLog.Lines.Add(Trim('Aguardando conexão...'));
+
+  PreencherComboBoxComArquivoJSON(ExtractFilePath(Application.ExeName) +
+    'pacotes.json');
+end;
+
+procedure TfrmPrincipal.PreencherComboBoxComArquivoJSON(const FileName: string);
+var
+  JSONObject: TJSONObject;
+  PackagesArray: TJSONArray;
+  I: Integer;
+  PackageObject: TJSONObject;
+  PackageName, PackageID: string;
+begin
+  // Verifica se o arquivo existe
+  if FileExists(FileName) then
+  begin
+    // Lê o conteúdo do arquivo para uma string
+    JSONObject := TJSONObject.ParseJSONValue(TFile.ReadAllText(FileName))
+      as TJSONObject;
+
+    try
+      // Limpa a lista de itens do ComboBox
+      cbbPacotes.Items.Clear;
+
+      // Verifica se o JSON contém a chave "packages"
+      if JSONObject.TryGetValue<TJSONArray>('packages', PackagesArray) then
+      begin
+        // Preenche o ComboBox com os itens do array JSON
+        for I := 0 to PackagesArray.Count - 1 do
+        begin
+          // Obtém o objeto JSON para cada pacote
+          PackageObject := PackagesArray.Items[I] as TJSONObject;
+
+          // Verifica se o objeto contém as chaves "nome" e "pacote"
+          if PackageObject.TryGetValue<string>('nome', PackageName) and
+            PackageObject.TryGetValue<string>('pacote', PackageID) then
+          begin
+            // Adiciona o valor ao ComboBox usando o formato "nome (pacote)"
+            cbbPacotes.Items.Add(Format('%s (%s)', [PackageName, PackageID]));
+          end
+          else
+          begin
+            ShowMessage('Objeto de pacote inválido no arquivo JSON.');
+            Exit;
+          end;
+        end;
+      end
+      else
+        ShowMessage('O arquivo pacotes.json não contém a chave "packages".');
+    finally
+      // Libera a memória alocada para o objeto JSON
+      JSONObject.Free;
+    end;
+  end
+  else
+    ShowMessage('O arquivo pacotes.json não foi encontrado.');
+end;
+
+procedure TfrmPrincipal.Timer1Timer(Sender: TObject);
+begin
+  StatusBar1.Panels[0].Text := 'Data: ' + DateToStr(Date) + ' Hora: ' +
+    TimeToStr(Time);
+end;
+
+end.
